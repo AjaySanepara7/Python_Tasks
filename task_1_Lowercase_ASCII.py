27c8018fac9f6a0b51369533d02a29b64254a321
AjaySanepara7 <ajaysanepara7@gmail.com>
2024-09-26 19:33:38 +0530

    fix: Removed while loop

--- task_1_Lowercase_ASCII.py
+++ task_1_Lowercase_ASCII.py
@@ -1,16 +1,16 @@
 name = input("Enter your name: ")
 
 def verify_name(name):
-    print('Please enter an alphabetical name')
-    name = input("Enter your name: ")
-
     if name.isalpha() == False:
-        verify_name(name)  
+        print('Please enter an alphabetical name')
+        name = input("Enter your name: ")
+        return verify_name(name)  
     else:
         return name
+
     
-verify_name(name)
+name = verify_name(name)
         
 for z in name:
     if z.islower():
-        print(f"The ASCII value of {z} is: {ord(z)}")
+        print(f"{z} : {ord(z)}")
