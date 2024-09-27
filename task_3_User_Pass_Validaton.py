9b1941df6326d0b84b5bbdec01f519d810854825
AjaySanepara7 <ajaysanepara7@gmail.com>
2024-09-27 11:53:22 +0530

    fix: Removed if else statement

--- task_3_User_Pass_Validaton.py
+++ task_3_User_Pass_Validaton.py
@@ -1,26 +1,19 @@
 import re
 
-
-username = input("Enter username of your choise: ")
-
+username = input("Enter username of your choice: ")
 
 is_username_valid = 0
+conditions = [
+    ("Username sholud atleast contain 8 letters",len(username) >= 8),
+    ("Username sholud have atleast one '.'", "." in username),
+    ("Username sholud have atleast one '_'", "_" in username),
+]
 
-if len(username) < 8:
-    print("Username sholud atleast contain 8 letters")
-    is_username_valid = 1
-
-if not re.search("\.", username):
-    print("Username sholud have atleast one '.'")
-    is_username_valid = 1
-
-if not re.search("_", username):
-    print("Username sholud have atleast one '_'")
-    is_username_valid = 1
-    
-if is_username_valid == 0:
-    print("Valid Username")
-
+for x in conditions:
+    for y in x:
+        if (y) == False:
+            print(x[0])
+            is_username_valid = 1
 
         
 while is_username_valid == 1:
@@ -28,42 +21,34 @@ while is_username_valid == 1:
     username = input("Enter a valid username: ")
     is_username_valid = 0
 
-    if len(username) < 8:
-        print("Username sholud atleast contain 8 letters")
-        is_username_valid = 1
+    conditions = [
+    ("Username sholud atleast contain 8 letters",len(username) >= 8),
+    ("Username sholud have atleast one '.'", "." in username),
+    ("Username sholud have atleast one '_'", "_" in username),
+    ]
 
-    if not re.search("\.", username):
-        print("Username sholud have atleast one '.'")
-        is_username_valid = 1
+    for condition in conditions:
+        for message in condition:
+            if (message) == False:
+                print(condition[0])
+                is_username_valid = 1
 
-    if not re.search("_", username):
-        print("Username sholud have atleast one '_'")
-        is_username_valid = 1
-
-
-
+   
 password = input("Enter password: ")
-
 is_password_valid = 0
 
-if not re.search("[@#$%]", password):
-    print("Password should contain atleast one of these character @,#,$,%")
-    is_password_valid = 1
+pass_conditions = [
+    ("Password should contain atleast one of these character @,#,$,%", bool(re.search("[@#$%]", password))),
+    ("Password sholud have atleast one lowercase letter", bool(re.search("[a-z]", password))),
+    ("Password sholud have atleast one uppercase letter", bool(re.search("[A-Z]", password))),
+    ("Password should contain atleast one digit from 0-9", bool(re.search("[0-9]", password))),
+]
 
-if not re.search("[a-z]", password):
-    print("Password sholud have atleast one lowercase letter")
-    is_password_valid = 1
-
-if not re.search("[A-Z]", password):
-    print("Password sholud have atleast one uppercase letter")
-    is_password_valid = 1
-
-if not re.search("[0-9]", password):
-    print("Password should contain atleast one digit from 0-9")
-    is_password_valid = 1
-
-if is_password_valid == 0:
-    print("Valid Password")
+for condition in pass_conditions:
+    for message in condition:
+        if message == False:
+            print(condition[0])
+            is_password_valid = 1
 
 
 while is_password_valid == 1:
@@ -71,21 +56,15 @@ while is_password_valid == 1:
     password = input("Enter a valid password: ")
     is_password_valid = 0
 
-    if not re.search("[@#$%]", password):
-        print("Enter atleast one of these character @,#,$,%")
-        is_password_valid = 1
+    pass_conditions = [
+    ("Password should contain atleast one of these character @,#,$,%", bool(re.search("[@#$%]", password))),
+    ("Password sholud have atleast one lowercase letter", bool(re.search("[a-z]", password))),
+    ("Password sholud have atleast one uppercase letter", bool(re.search("[A-Z]", password))),
+    ("Password should contain atleast one digit from 0-9", bool(re.search("[0-9]", password))),
+    ]
 
-    if not re.search("[a-z]", password):
-        print("Password sholud have atleast one lowercase letter")
-        is_password_valid = 1
-
-    if not re.search("[A-Z]", password):
-        print("Password sholud have atleast one uppercase letter")
-        is_password_valid = 1
-
-    if not re.search("[0-9]", password):
-        print("Password should contain atleast one digit from 0-9")
-        is_password_valid = 1
-
-
-      
+    for condition in pass_conditions:
+        for message in condition:
+            if message == False:
+                print(condition[0])
+                is_password_valid = 1
