5b6b281465ec252303bb3e0b2e05b709f69dab08
AjaySanepara7 <ajaysanepara7@gmail.com>
2024-09-27 12:02:01 +0530

    fix: Add while loop and change input type

--- task_5_Phone_Directory.py
+++ task_5_Phone_Directory.py
@@ -3,7 +3,10 @@ namelist = []
 numberlist = []
 
 name = input("Enter name: ")
-number = int(input("Enter number: "))
+number = input("Enter number: ")
+
+while number.isnumeric() == False:
+    number = input("Invalid input Enter a numeric value: ")
 
 choice = input("Press 1 to enter another name & number \nPress any other key to exit: ")
 
@@ -12,7 +15,9 @@ numberlist.append(f"{number}")
 
 while choice == "1":
     name = input("Enter name: ")
-    number = int(input("Enter number: "))
+    number = input("Enter number: ")
+    while number.isnumeric() == False:
+        number = input("Invalid input Enter a numeric value: ")
     choice = input("Press 1 to enter another name & number \nPress 0 to exit: ")
     namelist.append(f"{name}")
     numberlist.append(f"{number}")
@@ -21,16 +26,16 @@ for k,v in zip(namelist, numberlist):
     phoneDirectory.update({k: v})
 
 
-operation_choice = int(input("Press 1 to search contact \nPress 2 to update contact \nPress 0 to exit: "))
+operation_choice = input("Press 1 to search contact \nPress 2 to update contact \nPress 0 to exit: ")
 
-while operation_choice == 1 or operation_choice == 2:
-    if operation_choice == 1:
+while operation_choice == "1" or operation_choice == "2":
+    if operation_choice == "1":
         searchInput = input("Enter name or number to search contact: ")
         for x,y in phoneDirectory.items():
             if searchInput == f"{x}" or searchInput == f"{y}":
                 print(x + ": " + y)
         operation_choice = int(input("Press 1 to search contact \nPress 2 to update contact \nPress 0 to exit: "))
-    elif operation_choice == 2:
+    elif operation_choice == "2":
         updateInput = input("Enter name or number where you want to update the contact: ")
         for x,y in list(phoneDirectory.items()):
             if updateInput == f"{x}" or updateInput == f"{y}":
