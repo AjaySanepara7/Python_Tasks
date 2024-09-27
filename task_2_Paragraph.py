6107832c6ccd84a883f53ef104ffd153a927e303
AjaySanepara7 <ajaysanepara7@gmail.com>
2024-09-27 11:50:35 +0530

    refactor: Changed variable name

--- task_2_Paragraph.py
+++ task_2_Paragraph.py
@@ -4,9 +4,9 @@ paragraph = input("Enter paragraph: ")
 
 print("Do you want to edit or update any word in the paragraph")
 
-x = input("Proceed (y/n)? ")
+choice = input("Proceed (y/n)? ")
 
-if x == "y" or x == "Y":
+if choice == "y" or choice == "Y":
     word = input("Enter the word you want to replace: ")
     new_word = input("Enter the word you want to replace it with or keep it empty if you want to delete the word: ")
     if new_word == "":
